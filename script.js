c10348cc266969e7ec7d2a1adeecbdf4bde110db
Abdul Shabbir <abdulqshabbir@gmail.com>
2017-12-25 13:15:44 +0300

    color change on click for each pad

--- script.js
+++ script.js
@@ -9,24 +9,56 @@ const sound3 = new Audio('sounds/simonSound3.mp3');
 const sound4 = new Audio('sounds/simonSound4.mp3');
 
 
+
+
 greenPad.addEventListener('click', function(e) {
   e.preventDefault();
   sound1.currentTime = 0;
   sound1.play();
+  greenPad.classList.remove('activeGreen');
+  greenPad.classList.add('activeGreen');
+  greenPad.addEventListener('transitionend', removeActiveGreenClass);
 });
 
 bluePad.addEventListener('click', function(e) {
   e.preventDefault();
   sound2.currentTime = 0;
   sound2.play();
+  greenPad.classList.remove('activeBlue');
+  bluePad.classList.add('activeBlue');
+  bluePad.addEventListener('transitionend', removeActiveBlueClass);
 });
+
 redPad.addEventListener('click', function(e) {
   e.preventDefault();
   sound3.currentTime = 0;
   sound3.play();
+  greenPad.classList.remove('activeRed');
+  redPad.classList.add('activeRed');
+  redPad.addEventListener('transitionend', removeActiveRedClass);
 });
+
 yellowPad.addEventListener('click', function(e) {
   e.preventDefault();
   sound4.currentTime = 0;
   sound4.play();
+  greenPad.classList.remove('activeYellow');
+  yellowPad.classList.add('activeYellow');
+  yellowPad.addEventListener('transitionend', removeActiveYellowClass);
 });
+
+function removeActiveGreenClass() {
+  this.classList.remove('activeGreen');
+}
+
+function removeActiveYellowClass() {
+  this.classList.remove('activeYellow');
+}
+
+function removeActiveRedClass() {
+  this.classList.remove('activeRed');
+}
+
+function removeActiveBlueClass() {
+  this.classList.remove('activeBlue');
+}
